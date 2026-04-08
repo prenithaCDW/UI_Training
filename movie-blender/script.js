b1e76cb822fecf7d0620406c5d2da68396d38105
prenithaCDW <prenitha.sp@cdw.com>
2026-04-08 12:39:47 +0530

    added thumbnail and play pause logic

--- movie-blender/script.js
+++ movie-blender/script.js
@@ -2,41 +2,43 @@ const videoElement = document.querySelector(".video-container");
 const titleElement = document.querySelector(".movie-title");
 const descriptionElement = document.querySelector(".movie-description");
 const movieSection = document.querySelector(".movies-container");
-const commentsContainer=document.querySelector(".comments-container");
+const commentsContainer = document.querySelector(".comments-container");
+const playBtn = document.querySelector(".play-btn");
+const icon = playBtn.querySelector("i");
 async function loadMovieData() {
-    try {
-        const response = await fetch("./data/video.json");
-        const dataMovie = await response.json();
-        videoElement.src = dataMovie.videoUrl;
-        titleElement.textContent = dataMovie.title;
-        descriptionElement.textContent = dataMovie.description;
+  try {
+    const response = await fetch("./data/video.json");
+    const dataMovie = await response.json();
+    videoElement.src = dataMovie.videoUrl;
+    titleElement.textContent = dataMovie.title;
+    descriptionElement.textContent = dataMovie.description;
 
-        dataMovie.comments.forEach(commentData => {
-            const divComment=document.createElement("div");
-            divComment.className="comment-content";
-            const img = document.createElement("img");
-            img.className = "comment-image";
-            img.src = commentData.image;
-            img.alt = commentData.name;
-            const divContent=document.createElement("div");
-            divContent.className="content";
-            const name = document.createElement("h5");
-            name.className = "comment-name";
-            name.textContent = commentData.name;
-            const comment = document.createElement("p");
-            comment.className = "comment";
-            comment.textContent = commentData.comment;
+    dataMovie.comments.forEach(commentData => {
+      const divComment = document.createElement("div");
+      divComment.className = "comment-content";
+      const img = document.createElement("img");
+      img.className = "comment-image";
+      img.src = commentData.image;
+      img.alt = commentData.name;
+      const divContent = document.createElement("div");
+      divContent.className = "content";
+      const name = document.createElement("h5");
+      name.className = "comment-name";
+      name.textContent = commentData.name;
+      const comment = document.createElement("p");
+      comment.className = "comment";
+      comment.textContent = commentData.comment;
 
-            divComment.appendChild(img);
-            divContent.appendChild(name);
-            divContent.appendChild(comment);
-            divComment.appendChild(divContent)
-            commentsContainer.appendChild(divComment)
-            movieSection.appendChild(commentsContainer);
-        });
-    } catch (error) {
-        console.error("Error loading movie data:", error);
-    }
+      divComment.appendChild(img);
+      divContent.appendChild(name);
+      divContent.appendChild(comment);
+      divComment.appendChild(divContent)
+      commentsContainer.appendChild(divComment)
+      movieSection.appendChild(commentsContainer);
+    });
+  } catch (error) {
+    console.error("Error loading movie data:", error);
+  }
 }
 loadMovieData();
 
@@ -51,8 +53,8 @@ async function displayPoster() {
       posterCard.className = 'posters-card';
       const posterImage = document.createElement('img');
       posterImage.src = poster.imageUrl;
-      posterImage.className='posters-image';
-      posterImage.alt=poster.title;
+      posterImage.className = 'posters-image';
+      posterImage.alt = poster.title;
       posterCard.appendChild(posterImage);
       posterContainer.appendChild(posterCard);
     });
@@ -61,3 +63,21 @@ async function displayPoster() {
   }
 }
 displayPoster();
+
+playBtn.addEventListener("click", () => {
+  if (videoElement.paused) {
+    videoElement.play();
+  } else {
+    videoElement.pause();
+  }
+});
+
+videoElement.addEventListener("play", () => {
+    icon.classList.remove("fa-play");
+    icon.classList.add("fa-pause");
+});
+
+videoElement.addEventListener("pause", () => {
+    icon.classList.remove("fa-pause");
+    icon.classList.add("fa-play");
+});
