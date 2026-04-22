a65c6b5cf5b8d2f999e68f9700d668cfe78656c9
prenithaCDW <prenitha.sp@cdw.com>
2026-04-22 17:26:06 +0530

    anonymous function in Banking

--- banking-service/script.js
+++ banking-service/script.js
@@ -1,49 +1,50 @@
-//anonymous function
-const Bank = (function () {
-    const accounts = [
-        { accountNumber: "1001", balance: 25000, cards: [{ cardNumber: "1111-2222", pin: "1234" }, { cardNumber: "3333-4444", pin: "2345" }] },
-        { accountNumber: "1003", balance: 8000, cards: [{ cardNumber: "5555-6666", pin: "3456" }] },
-        { accountNumber: "1004", balance: 20000, cards: [{ cardNumber: "7777-8888", pin: "4567" }] },
-        { accountNumber: "1005", balance: 5000, cards: [{ cardNumber: "9999-0000", pin: "5678" }] }
-    ];
-    //verify account
-    function verifyAccount(accountNumber) {
-        return accounts.find(account => account.accountNumber === accountNumber);
-    }
-
-    //ATM object function
-    const ATM = {
-        //withdraw
-        withdraw: function (account, amount) {
-            console.log("entered")
-            if (isNaN(amount) || amount <= 0) {
-                return "Invalid amount";
-            }
-            if (amount > account.balance) {
-                return "Insufficient balance";
-            }
-            account.balance -= amount;
-            return `Withdrawal was successful. The balance amount is ${account.balance}`;
-        },
-        //deposit
-        deposit: function (account, amount) {
-            if (isNaN(amount) || amount <= 0) {
-                return "Invalid amount";
-            }
-            account.balance += amount;
-            return `Deposit was successful. The balance amount is ${account.balance}`;
-        },
-        balance: function (account) {
-            return `The balance in account is ${account.balance}`;
-        },
-        verifyAccount,
-    }
-    return { ATM };
-    // Immediately Invoked Function Expression
-})();
-
 //choices
 function Banking() {
+
+    //anonymous function
+    const Bank = (function () {
+        const accounts = [
+            { accountNumber: "1001", balance: 25000, cards: [{ cardNumber: "1111-2222", pin: "1234" }, { cardNumber: "3333-4444", pin: "2345" }] },
+            { accountNumber: "1003", balance: 8000, cards: [{ cardNumber: "5555-6666", pin: "3456" }] },
+            { accountNumber: "1004", balance: 20000, cards: [{ cardNumber: "7777-8888", pin: "4567" }] },
+            { accountNumber: "1005", balance: 5000, cards: [{ cardNumber: "9999-0000", pin: "5678" }] }
+        ];
+        //verify account
+        function verifyAccount(accountNumber) {
+            return accounts.find(account => account.accountNumber === accountNumber);
+        }
+
+        //ATM object function
+        const ATM = {
+            //withdraw
+            withdraw: function (account, amount) {
+                console.log("entered")
+                if (isNaN(amount) || amount <= 0) {
+                    return "Invalid amount";
+                }
+                if (amount > account.balance) {
+                    return "Insufficient balance";
+                }
+                account.balance -= amount;
+                return `Withdrawal was successful. The balance amount is ${account.balance}`;
+            },
+            //deposit
+            deposit: function (account, amount) {
+                if (isNaN(amount) || amount <= 0) {
+                    return "Invalid amount";
+                }
+                account.balance += amount;
+                return `Deposit was successful. The balance amount is ${account.balance}`;
+            },
+            //balance
+            balance: function (account) {
+                return `The balance in account is ${account.balance}`;
+            },
+            verifyAccount,
+        }
+        return { ATM };
+        // Immediately Invoked Function Expression
+    })();
     while (true) {
         const accountNumber = window.prompt("Enter account number: ");
 
